0a7b7ef2979324817fdff951c514e93a6028d345
Glauber Santana <glauber.silva14@gmail.com>
2024-08-26 15:33:52 -0300

    feat(plugins): add harpoon

---
+++ config/plugins/harpoon/default.nix
@@ -0,0 +1,17 @@
+_: {
+  plugins.harpoon = {
+    enable = true;
+    enableTelescope = true;
+    keymapsSilent = true;
+    keymaps = {
+      addFile = "<leader>a";
+      toggleQuickMenu = "<C-e>";
+      navFile = {
+        "1" = "<C-1>";
+        "2" = "<C-2>";
+        "3" = "<C-3>";
+        "4" = "<C-4>";
+      };
+    };
+  };
+}
