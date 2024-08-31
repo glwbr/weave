dda7e0622d8c97e5e08c0305f98acd1fe034c8ab
Glauber Santana <glauber.silva14@gmail.com>
2024-08-31 04:16:03 -0300

    feat(nvim-lint): add stylint, hadolint

--- config/plugins/treesitter/nvim-lint.nix
+++ config/plugins/treesitter/nvim-lint.nix
@@ -1,16 +1,19 @@
 {pkgs, ...}: {
   extraPackages = with pkgs; [
     golangci-lint
-
     eslint_d
+    hadolint
     nodePackages.jsonlint
     selene
     statix
+    stylelint
   ];
 
   plugins.lint = {
     enable = true;
     lintersByFt = {
+      css = ["stylelint"];
+      docker = ["hadolint"];
       nix = ["statix"];
       go = ["golangci-lint"];
       lua = ["selene"];
