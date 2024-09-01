7f9b0626298d6de5094bcf8fa51027746f41b9d1
Glauber Santana <glauber.silva14@gmail.com>
2024-09-01 18:17:10 -0300

    feat(luasnip): add snippet expansion function

--- config/plugins/completion/snippets/luasnip.nix
+++ config/plugins/completion/snippets/luasnip.nix
@@ -3,7 +3,7 @@ _: {
     friendly-snippets.enable = true;
     luasnip = {
       enable = true;
-      #fromLua = [{paths = ./.;}];
+      # fromLua = [{paths = ./lua;}];
       fromVscode = [
         {
           lazyLoad = true;
@@ -12,4 +12,55 @@ _: {
       ];
     };
   };
+
+  extraConfigLua =
+    # lua
+    ''
+      local ls = require "luasnip"
+      vim.snippet.expand = ls.lsp_expand
+
+      vim.snippet.active = function(filter)
+        filter = filter or {}
+        filter.direction = filter.direction or 1
+
+        if filter.direction == 1 then
+          return ls.expand_or_jumpable()
+        else
+          return ls.jumpable(filter.direction)
+        end
+      end
+
+      vim.snippet.jump = function(direction)
+        if direction == 1 then
+          if ls.expandable() then
+            return ls.expand_or_jump()
+          else
+            return ls.jumpable(1) and ls.jump(1)
+          end
+        else
+          return ls.jumpable(-1) and ls.jump(-1)
+        end
+      end
+
+      vim.snippet.stop = ls.unlink_current
+
+      --WARN:a temporary way of defining commands for nixvim keymaps
+      -- since it would be a verbose way pass directly to the mkKeymap
+      --TODO: move this to a lua function folder?
+      vim.api.nvim_create_user_command('JumpOrExpand', function()
+        return vim.snippet.active { direction = 1 } and vim.snippet.jump(1)
+      end, {})
+
+      vim.api.nvim_create_user_command('JumpBackwards', function()
+        return vim.snippet.active { direction = -1 } and vim.snippet.jump(-1)
+      end, {})
+
+      -- vim.keymap.set({ "i", "s" }, "<C-k>", function()
+      --   return vim.snippet.active { direction = 1 } and vim.snippet.jump(1)
+      -- end, { silent = true })
+
+      -- vim.keymap.set({ "i", "s" }, "<C-j>", function()
+      --   return vim.snippet.active { direction = -1 } and vim.snippet.jump(-1)
+      -- end, { silent = true })
+    '';
 }
