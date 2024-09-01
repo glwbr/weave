ad985f4ac54011c4000cc67cae673fdc1a87629d
Glauber Santana <glauber.silva14@gmail.com>
2024-09-01 09:59:44 -0300

    feat(cmp): add copilot-cmp

--- config/plugins/completion/copilot.nix
+++ config/plugins/completion/copilot.nix
@@ -1,15 +1,18 @@
 _: {
-  plugins.copilot-lua = {
-    enable = true;
-    suggestion.enabled = false;
-    panel.enabled = false;
-    filetypes = {
-      yaml = false;
-      markdown = false;
-      help = false;
-      gitcommit = false;
-      gitrebase = false;
-      "." = false;
+  plugins = {
+    copilot-cmp.enable = true;
+    copilot-lua = {
+      enable = true;
+      suggestion.enabled = false;
+      panel.enabled = false;
+      filetypes = {
+        yaml = false;
+        markdown = false;
+        help = false;
+        gitcommit = false;
+        gitrebase = false;
+        "." = false;
+      };
     };
   };
 }
